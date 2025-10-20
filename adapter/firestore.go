feaf5835357f7bc8c8bb87993ed00f1c77282f6e
yasintuncerr <yasintuncerr@gmail.com>
2025-10-21 00:17:27 +0300

    Refactor Firestore adapter: Enhance Create, Read, Update, Delete, and Set methods to handle ID generation and improve error handling

--- adapter/firestore.go
+++ adapter/firestore.go
@@ -4,9 +4,52 @@ import (
 	"context"
 
 	"cloud.google.com/go/firestore"
+	"google.golang.org/api/iterator"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	repo "github.com/histopathai/main-service/internal/repository"
 )
+
+type FirestoreTransctionAdapter struct {
+	client *firestore.Client
+	tx     *firestore.Transaction
+}
+
+func (fta *FirestoreTransctionAdapter) Create(col string, data map[string]interface{}) (string, error) {
+	id, ok := data["id"].(string)
+	if !ok || id == "" {
+		id = fta.client.Collection(col).NewDoc().ID
+		data["id"] = id
+	}
+
+	err := fta.tx.Set(fta.client.Collection(col).Doc(id), data)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+func (fta *FirestoreTransctionAdapter) Read(col string, docID string) (map[string]interface{}, error) {
+	doc, err := fta.tx.Get(fta.client.Collection(col).Doc(docID))
+	if err != nil {
+		return nil, err
+	}
+	return doc.Data(), nil
+}
+
+func (fta *FirestoreTransctionAdapter) Update(col string, docID string, updates map[string]interface{}) error {
+	return fta.tx.Set(fta.client.Collection(col).Doc(docID), updates, firestore.MergeAll)
+}
+
+func (fta *FirestoreTransctionAdapter) Delete(col string, docID string) error {
+	return fta.tx.Delete(fta.client.Collection(col).Doc(docID))
+}
+
+func (fta *FirestoreTransctionAdapter) Set(col string, docID string, data map[string]interface{}) error {
+	return fta.tx.Set(fta.client.Collection(col).Doc(docID), data)
+}
 
 type FirestoreAdapter struct {
 	client *firestore.Client
@@ -18,75 +61,88 @@ func NewFirestoreAdapter(client *firestore.Client) *FirestoreAdapter {
 	}
 }
 
-func (fa *FirestoreAdapter) Create(ctx context.Context, collection string, data map[string]interface{}) (string, error) {
-	if id, ok := data["id"].(string); ok && id != "" {
-		docRef := fa.client.Collection(collection).Doc(id)
-		_, err := docRef.Set(ctx, data)
-		if err != nil {
-			return "", err
-		}
-		return docRef.ID, nil
+func (f *FirestoreAdapter) Create(ctx context.Context, col string, data map[string]interface{}) (string, error) {
+	id, ok := data["id"].(string)
+	if !ok || id == "" {
+		id = f.client.Collection(col).NewDoc().ID
+		data["id"] = id
 	}
-	docRef, _, err := fa.client.Collection(collection).Add(ctx, data)
+
+	_, err := f.client.Collection(col).Doc(id).Set(ctx, data)
 	if err != nil {
 		return "", err
 	}
-	return docRef.ID, nil
+
+	return id, nil
 }
 
-func (fa *FirestoreAdapter) Read(ctx context.Context, collection, docID string) (map[string]interface{}, error) {
-	docRef := fa.client.Collection(collection).Doc(docID)
-	docSnap, err := docRef.Get(ctx)
+func (f *FirestoreAdapter) Read(ctx context.Context, col, docID string) (map[string]interface{}, error) {
+	doc, err := f.client.Collection(col).Doc(docID).Get(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return docSnap.Data(), nil
+	return doc.Data(), nil
 }
 
-func (fa *FirestoreAdapter) Update(ctx context.Context, collection, docID string, updates map[string]interface{}) error {
-	docRef := fa.client.Collection(collection).Doc(docID)
-	_, err := docRef.Set(ctx, updates, firestore.MergeAll)
+func (f *FirestoreAdapter) Update(ctx context.Context, col, docID string, updates map[string]interface{}) error {
+	_, err := f.client.Collection(col).Doc(docID).Set(ctx, updates, firestore.MergeAll)
 	return err
 }
 
-func (fa *FirestoreAdapter) Delete(ctx context.Context, collection, docID string) error {
-	docRef := fa.client.Collection(collection).Doc(docID)
-	_, err := docRef.Delete(ctx)
+func (f *FirestoreAdapter) Delete(ctx context.Context, col, docID string) error {
+	_, err := f.client.Collection(col).Doc(docID).Delete(ctx)
 	return err
 }
 
-func (fa *FirestoreAdapter) Query(ctx context.Context, collection string, filters map[string]interface{}) ([]map[string]interface{}, error) {
-	colRef := fa.client.Collection(collection)
-	query := colRef.Query
+func (f *FirestoreAdapter) Set(ctx context.Context, col, docID string, data map[string]interface{}) error {
+	_, err := f.client.Collection(col).Doc(docID).Set(ctx, data)
+	return err
+}
+
+func (f *FirestoreAdapter) Query(ctx context.Context, col string, filters map[string]interface{}, pagination repo.Pagination) (*repo.QueryResult, error) {
+	query := f.client.Collection(col).Query
 
 	for field, value := range filters {
 		query = query.Where(field, "==", value)
 	}
 
+	if pagination.Offset > 0 {
+		query = query.Offset(pagination.Offset)
+	}
+	if pagination.Limit > 0 {
+		query = query.Limit(pagination.Limit)
+	}
+
 	iter := query.Documents(ctx)
 	defer iter.Stop()
 
-	docs, err := iter.GetAll()
-	if err != nil {
-		return nil, err
+	results := make([]map[string]interface{}, 0)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, doc.Data())
 	}
 
-	results := make([]map[string]interface{}, 0, len(docs))
-	for _, d := range docs {
-		results = append(results, d.Data())
-	}
-	return results, nil
+	return &repo.QueryResult{
+		Data:    results,
+		Limit:   pagination.Limit,
+		Offset:  pagination.Offset,
+		HasMore: len(results) == pagination.Limit,
+	}, nil
 }
 
-func (fa *FirestoreAdapter) Exists(ctx context.Context, collection, docID string) (bool, error) {
-	docRef := fa.client.Collection(collection).Doc(docID)
-	docSnap, err := docRef.Get(ctx)
+func (f *FirestoreAdapter) Exists(ctx context.Context, col, docID string) (bool, error) {
+	_, err := f.client.Collection(col).Doc(docID).Get(ctx)
 	if err != nil {
 		if status.Code(err) == codes.NotFound {
 			return false, nil
 		}
 		return false, err
 	}
-
-	return docSnap.Exists(), nil
+	return true, nil
 }
