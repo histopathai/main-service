34aab2e3c6917bea7996f0552f6435a960d16857
yasintuncerr <yasintuncerr@gmail.com>
2025-10-23 00:50:35 +0300

    Refactor upload service to improve patient info handling and validation logic

--- internal/service/upload_service.go
+++ internal/service/upload_service.go
@@ -39,24 +39,22 @@ type SignedUrlResponse struct {
 type ImageInfo struct {
 	FileName  string `json:"file_name"`
 	Format    string `json:"format"`
-	Width     int    `json:"width"`
-	Height    int    `json:"height"`
 	SizeBytes int64  `json:"size_bytes"`
 }
 
-type PatientInfo struct {
-	PatientID string  `json:"id,omitempty"` // optional, will be created if empty
-	Age       *int    `json:"age,omitempty"`
-	Gender    *string `json:"gender,omitempty"`
-	Race      *string `json:"race,omitempty"`
-	Disease   *string `json:"disease,omitempty"`
-	History   *string `json:"history,omitempty"`
+type NewPatientInfo struct {
+	Age     *int    `json:"age,omitempty"`
+	Gender  *string `json:"gender,omitempty"`
+	Race    *string `json:"race,omitempty"`
+	Disease *string `json:"disease,omitempty"`
+	History *string `json:"history,omitempty"`
 }
 
 type UploadImageRequest struct {
-	ImageInfo   ImageInfo   `json:"image_info"`
-	PatientInfo PatientInfo `json:"patient_info"`
-	WorkspaceID string      `json:"workspace_id"`
+	ImageInfo   ImageInfo      `json:"image_info"`
+	PatientInfo NewPatientInfo `json:"new_patient_info"`
+	PatientID   *string        `json:"patient_id,omitempty"`
+	WorkspaceID string         `json:"workspace_id"`
 }
 
 func MimeTypeFromFormat(format string) string {
@@ -90,20 +88,63 @@ func NewUploadService(
 	}
 }
 
-func (us *UploadService) validateUploadRequest(ctx context.Context, req *UploadImageRequest) error {
+func (us *UploadService) validateUploadRequest(ctx context.Context, req *UploadImageRequest) (*models.Image, *models.Patient, error) {
+	// Scope variables
+	var patient *models.Patient
+	var image *models.Image
+	var err error
 	details := make(map[string]interface{})
+
 	// Check workspace exists
-	result, err := us.repo.Read(ctx, string(WorkspaceCollection), req.WorkspaceID)
+	wr, err := us.repo.Read(ctx, string(WorkspaceCollection), req.WorkspaceID)
 	if err != nil {
-		return apperrors.NewInternalError("failed to read workspace during upload validation", err)
+		return nil, nil, apperrors.NewInternalError("failed to read workspace during upload validation", err)
 	}
-	if result == nil {
+	if wr == nil {
 		details["workspace"] = "required"
 	} else {
 		w := &models.Workspace{}
-		w.FromMap(result)
+		w.FromMap(wr)
 		if w.AnnotationTypeID == "" {
-			details["workspace"] = "workspace must have an annotation type assigned"
+			details["workspace"] = "workspace must have an annotation type assigned. Assign an annotation type before uploading images."
+		}
+	}
+
+	// Patient id or New patient info must be provided
+	if req.PatientID != nil {
+		// Verify patient exists
+		p, err := us.repo.Read(ctx, string(PatientCollection), *req.PatientID)
+		if err != nil {
+			return nil, nil, apperrors.NewInternalError("failed to read patient during upload validation", err)
+		}
+		if p == nil {
+			details["patient_id"] = "patient not found"
+		} else {
+			patient = &models.Patient{}
+			patient.FromMap(p)
+		}
+
+	} else {
+		// Verify at least one patient detail is provided
+		if req.PatientInfo.Age == nil && req.PatientInfo.Race == nil && req.PatientInfo.Gender == nil &&
+			req.PatientInfo.History == nil && req.PatientInfo.Disease == nil {
+			details["patient_info"] = "either patient ID or at least one patient detail must be provided"
+			details["patient_info.age"] = "optional"
+			details["patient_info.gender"] = "optional"
+			details["patient_info.race"] = "optional"
+			details["patient_info.disease"] = "optional"
+			details["patient_info.history"] = "optional"
+			details["patient_info.patient_id"] = "optional when patient details are provided"
+		} else {
+			// Create new patient object
+			patient = &models.Patient{
+				ID:      "",
+				Age:     req.PatientInfo.Age,
+				Gender:  req.PatientInfo.Gender,
+				Race:    req.PatientInfo.Race,
+				Disease: req.PatientInfo.Disease,
+				History: req.PatientInfo.History,
+			}
 		}
 	}
 
@@ -116,51 +157,73 @@ func (us *UploadService) validateUploadRequest(ctx context.Context, req *UploadI
 	} else if !models.IsImageFormatSupported(req.ImageInfo.Format) {
 		details["imageInfo.format"] = fmt.Sprintf("unsupported format: %s", req.ImageInfo.Format)
 	}
-	if req.ImageInfo.Width <= 0 {
-		details["imageInfo.width"] = "must be positive"
-	}
-	if req.ImageInfo.Height <= 0 {
-		details["imageInfo.height"] = "must be positive"
-	}
+
 	if req.ImageInfo.SizeBytes <= 0 {
 		details["imageInfo.size_bytes"] = "must be positive"
 	}
 
 	if len(details) > 0 {
-		us.logger.Info("Upload request validation failed", "details", details)
-		return apperrors.NewBadRequestError("invalid upload image request", details)
+		return nil, nil, apperrors.NewBadRequestError("invalid upload image request", details)
 	}
-	return nil
+
+	// Create image object
+	image = &models.Image{}
+
+	uuid := uuid.New().String()
+
+	image.FileName = req.ImageInfo.FileName
+	image.Format = req.ImageInfo.Format
+	image.SizeBytes = req.ImageInfo.SizeBytes
+	image.WorkspaceID = req.WorkspaceID
+	image.ID = fmt.Sprintf("%s-%s", uuid, req.ImageInfo.FileName)
+	image.OriginPath = fmt.Sprintf("gs://%s/%s", us.bucketName, image.ID)
+	image.PatientID = patient.ID
+
+	return image, patient, nil
 }
 
-func (us *UploadService) ValidateAndCreatePatient(info *PatientInfo) *models.Patient {
-	if info.Age == nil && info.Race == nil && info.Gender == nil &&
-		info.History == nil && info.Disease == nil {
-		return nil
+func (us *UploadService) GenerateSignedUploadURL(ctx context.Context, contentType string, image *models.Image, patient *models.Patient) (string, error) {
+
+	headers := []string{
+		"Content-Type:" + contentType,
+		"x-goog-meta-image-id:" + image.ID,
+		"x-goog-meta-workspace-id:" + image.WorkspaceID,
+		"x-goog-meta-creator-id:" + image.CreatorID,
+		"x-goog-meta-image-format:" + image.Format,
+		"x-goog-meta-image-size-bytes:" + fmt.Sprintf("%d", image.SizeBytes),
+		"x-goog-meta-image-filename:" + image.FileName,
+		"x-goog-meta-image-origin-path:" + image.OriginPath,
 	}
 
-	patient := &models.Patient{
-		Age:     info.Age,
-		Race:    info.Race,
-		Gender:  info.Gender,
-		History: info.History,
-		Disease: info.Disease,
+	if image.PatientID != "" {
+		headers = append(headers, "x-goog-meta-patient-id:"+image.PatientID)
+	} else {
+		if patient.Age != nil {
+			headers = append(headers, "x-goog-meta-patient-age:"+fmt.Sprintf("%d", *patient.Age))
+		}
+		if patient.Gender != nil {
+			headers = append(headers, "x-goog-meta-patient-gender:"+*patient.Gender)
+		}
+		if patient.Race != nil {
+			headers = append(headers, "x-goog-meta-patient-race:"+*patient.Race)
+		}
+		if patient.Disease != nil {
+			headers = append(headers, "x-goog-meta-patient-disease:"+*patient.Disease)
+		}
+		if patient.History != nil {
+			headers = append(headers, "x-goog-meta-patient-history:"+*patient.History)
+		}
 	}
-
-	return patient
-}
-
-func (us *UploadService) GenerateSignedUploadURL(ctx context.Context, fileName string, contentType string) (string, error) {
 
 	opts := &storage.SignedURLOptions{
 		Scheme:      storage.SigningSchemeV4,
 		Method:      "PUT",
 		Expires:     time.Now().Add(30 * time.Minute),
 		ContentType: contentType,
-		Headers:     []string{fmt.Sprintf("Content-Type:%s", contentType)},
+		Headers:     headers,
 	}
 
-	u, err := us.storageClient.Bucket(us.bucketName).SignedURL(fileName, opts)
+	u, err := us.storageClient.Bucket(us.bucketName).SignedURL(image.ID, opts)
 	if err != nil {
 		return "", apperrors.NewInternalError("failed to generate signed URL", err)
 	}
@@ -173,92 +236,22 @@ func (us *UploadService) ProcessUpload(ctx context.Context, req *UploadImageRequ
 	if !ok || CreatorID == "" {
 		return nil, apperrors.NewUnauthorizedError("missing user ID in context")
 	}
-
-	if err := us.validateUploadRequest(ctx, req); err != nil {
-		return nil, err
-	}
-
-	var imageID, patientID string
-	var fileName string
-
-	err := us.repo.RunTransaction(ctx, func(txCtx context.Context, tx repository.Transaction) error {
-
-		patientID = req.PatientInfo.PatientID
-
-		if patientID == "" {
-			patient := us.ValidateAndCreatePatient(&req.PatientInfo)
-			if patient != nil {
-				patient.CreatedAt = time.Now()
-				patient.UpdatedAt = time.Now()
-
-				id, err := tx.Create(string(PatientCollection), patient.ToMap())
-				if err != nil {
-					return apperrors.NewInternalError("failed to create patient transactionally", err)
-				}
-				patientID = id
-				us.logger.Info("Patient created in transaction", "patientID", patientID)
-
-			}
-
-		} else {
-			// Verify patient exists
-			_, err := tx.Read(string(PatientCollection), patientID)
-			if err != nil {
-				return apperrors.NewNotFoundError("patient not found")
-			}
-		}
-
-		imageID = uuid.New().String()
-		fileName = fmt.Sprintf("%s-%s", imageID, req.ImageInfo.FileName)
-
-		image := &models.Image{
-			ID:            imageID,
-			FileName:      fileName,
-			Format:        req.ImageInfo.Format,
-			Width:         req.ImageInfo.Width,
-			Height:        req.ImageInfo.Height,
-			SizeBytes:     req.ImageInfo.SizeBytes,
-			CreatorID:     CreatorID,
-			PatientID:     patientID,
-			WorkspaceID:   req.WorkspaceID,
-			OriginPath:    fmt.Sprintf("gs://%s/%s", us.bucketName, fileName),
-			ProcessedPath: "",
-			Status:        models.StatusUploadWaiting,
-			CreatedAt:     time.Now(),
-			UpdatedAt:     time.Now(),
-		}
-
-		imgID, err := tx.Create(string(ImageCollection), image.ToMap())
-		if err != nil {
-			return apperrors.NewInternalError("failed to create image record transactionally", err)
-		}
-		imageID = imgID
-
-		us.logger.Info("Image record created in transaction",
-			"image_id", imageID,
-			"patient_id", patientID,
-			"workspace_id", req.WorkspaceID,
-		)
-		return nil
-	})
-
+	// Validate request
+	image, patient, err := us.validateUploadRequest(ctx, req)
 	if err != nil {
-		us.logger.Error("Transaction failed during upload process", "error", err)
 		return nil, err
 	}
+	image.CreatorID = CreatorID
 
-	//Generate signed URL
 	contentType := MimeTypeFromFormat(req.ImageInfo.Format)
-	uploadURL, err := us.GenerateSignedUploadURL(ctx, fileName, contentType)
+	UploadURL, err := us.GenerateSignedUploadURL(ctx, contentType, image, patient)
 	if err != nil {
 		return nil, err
 	}
 
-	resp := &SignedUrlResponse{
-		UploadURL: uploadURL,
-		ImageID:   imageID,
+	return &SignedUrlResponse{
+		ImageID:   image.ID,
+		UploadURL: UploadURL,
 		ExpiresAt: time.Now().Add(30 * time.Minute).Unix(),
-	}
-
-	return resp, nil
+	}, nil
 }
