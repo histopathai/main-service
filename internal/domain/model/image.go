83be1d5fb9f9f808b9de43d7436cab09b026601e
yasintuncerr <yasintuncerr@gmail.com>
2025-10-24 17:48:19 +0300

    Add Image model and status constants for image processing

---
+++ internal/domain/model/image.go
@@ -0,0 +1,28 @@
+package model
+
+import "time"
+
+type ImageStatus string
+
+const (
+	StatusUploaded   ImageStatus = "UPLOADED"
+	StatusProcessing ImageStatus = "PENDING"
+	StatusProcessed  ImageStatus = "PROCESSED"
+	StatusFailed     ImageStatus = "FAILED"
+)
+
+type Image struct {
+	ID            string
+	PatientID     string
+	CreatorID     string
+	FileName      string
+	Format        string
+	Width         int
+	Height        int
+	Size          int64
+	OriginPath    string
+	ProcessedPath *string
+	Status        ImageStatus
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
