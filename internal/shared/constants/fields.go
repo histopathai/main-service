bcf1e9c3103c7e55f8a44a7cd21db93f5e17c782
yasintuncerr <yasintuncerr@gmail.com>
2025-10-24 23:48:09 +0300

    Add constants for workspace, patient, image, annotation types, and timestamps

---
+++ internal/shared/constants/fields.go
@@ -0,0 +1,61 @@
+package constants
+
+const (
+	WorkspaceNameField             = "Name"
+	WorkspaceDescField             = "Desc"
+	WorkspaceOrganTypeField        = "OrganType"
+	WorkspaceOrganizationField     = "Organization"
+	WorkspaceLicenseField          = "License"
+	WorkspaceResourceURLField      = "ResourceURL"
+	WorkspaceReleaseYearField      = "ReleaseYear"
+	WorkspaceAnnotationTypeIDField = "AnnotationTypeID"
+)
+
+const (
+	PatientWorkspaceIDField = "WorkspaceID"
+	PatientAnonymNameField  = "AnonymName"
+	PatientAgeField         = "Age"
+	PatientGenderField      = "Gender"
+	PatientRaceField        = "Race"
+	PatientDiseaseField     = "Disease"
+	PatientSubtypeField     = "Subtype"
+	PatientGradeField       = "Grade"
+	PatientHistoryField     = "History"
+)
+
+const (
+	ImagePatientIDField     = "PatientID"
+	ImageCreatorIDField     = "CreatorID"
+	ImageFileNameField      = "FileName"
+	ImageFormatField        = "Format"
+	ImageWidthField         = "Width"
+	ImageHeightField        = "Height"
+	ImageSizeField          = "Size"
+	ImageOriginPathField    = "OriginPath"
+	ImageProcessedPathField = "ProcessedPath"
+	ImageStatusField        = "Status"
+)
+
+const (
+	AnnotationTypeNameField                  = "Name"
+	AnnotationTypeDescField                  = "Desc"
+	AnnotationTypeScoreEnabledField          = "ScoreEnabled"
+	AnnotationTypeScoreNameField             = "ScoreName"
+	AnnotationTypeScoreRangeField            = "ScoreRange"
+	AnnotationTypeClassificationEnabledField = "ClassificationEnabled"
+	AnnotationTypeClassListField             = "ClassList"
+)
+
+const (
+	AnnotationImageIDField          = "ImageID"
+	AnnotationAnnotatorIDField      = "AnnotatorID"
+	AnnotationAnnotationTypeIDField = "AnnotationTypeID"
+	AnnotationScoreField            = "Score"
+	AnnotationClassField            = "Class"
+	AnnotationPolygonField          = "Polygon"
+)
+
+const (
+	CreatedAtField = "CreatedAt"
+	UpdatedAtField = "UpdatedAt"
+)
