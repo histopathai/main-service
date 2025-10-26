1822d376a76d6f66d39f3d3fe3f605d562ffea19
yasintuncerr <yasintuncerr@gmail.com>
2025-10-26 20:44:47 +0300

    Fix JSON tag typo in AnonymName field for CreatePatientRequest and UpdatePatientRequest

--- internal/api/http/dto/request/patient.go
+++ internal/api/http/dto/request/patient.go
@@ -2,7 +2,7 @@ package request
 
 type CreatePatientRequest struct {
 	WorkspaceID string  `json:"workspace_id" binding:"required,uuid4" example:"550e8400-e29b-41d4-a716-446655440000"`
-	AnonymName  string  `jason:"anonym_name" binding:"required" example:"Patient_001"`
+	AnonymName  string  `json:"anonym_name" binding:"required" example:"Patient_001"`
 	Age         *int    `json:"age,omitempty" example:"45"`
 	Gender      *string `json:"gender,omitempty" example:"Female"`
 	Race        *string `json:"race,omitempty" example:"Asian"`
@@ -14,7 +14,7 @@ type CreatePatientRequest struct {
 
 type UpdatePatientRequest struct {
 	WorkspaceID *string `json:"workspace_id,omitempty" binding:"omitempty,uuid4" example:"550e8400-e29b-41d4-a716-446655440000"`
-	AnonymName  *string `jason:"anonym_name,omitempty" example:"Patient_001"`
+	AnonymName  *string `json:"anonym_name,omitempty" example:"Patient_001"`
 	Age         *int    `json:"age,omitempty" example:"45"`
 	Gender      *string `json:"gender,omitempty" example:"Female"`
 	Race        *string `json:"race,omitempty" example:"Asian"`
@@ -22,4 +22,9 @@ type UpdatePatientRequest struct {
 	Subtype     *string `json:"subtype,omitempty" example:"IDH-wildtype"`
 	Grade       *int    `json:"grade,omitempty" example:"3"`
 	History     *string `json:"history,omitempty" example:"No prior history of cancer."`
+}
+
+type ListPatientsByCriteriaRequest struct {
+	Filters    []JSONFilterRequest   `json:"filters,omitempty" binding:"omitempty,dive"`
+	Pagination JSONPaginationRequest `json:"pagination"`
 }
