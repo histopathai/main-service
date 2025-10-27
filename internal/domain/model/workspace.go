65f8a7ba0f2088c848a20295cfcf65e9f01030f5
yasintuncer <yasintuncerr@outlook.com>
2025-10-27 19:42:12 +0300

    Add getter and setter methods for CreatedAt and UpdatedAt fields in Workspace struct

--- internal/domain/model/workspace.go
+++ internal/domain/model/workspace.go
@@ -16,3 +16,19 @@ type Workspace struct {
 	CreatedAt        time.Time
 	UpdatedAt        time.Time
 }
+
+func (w Workspace) GetID() string {
+	return w.ID
+}
+
+func (w *Workspace) SetID(id string) {
+	w.ID = id
+}
+
+func (w *Workspace) SetCreatedAt(t time.Time) {
+	w.CreatedAt = t
+}
+
+func (w *Workspace) SetUpdatedAt(t time.Time) {
+	w.UpdatedAt = t
+}
