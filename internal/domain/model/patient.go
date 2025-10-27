d0aa54df95c7a93bb5812c9ebc4e33713d8e624d
yasintuncer <yasintuncerr@outlook.com>
2025-10-27 19:41:51 +0300

    Add getter and setter methods for ID, CreatedAt, and UpdatedAt fields in Patient struct

--- internal/domain/model/patient.go
+++ internal/domain/model/patient.go
@@ -16,3 +16,19 @@ type Patient struct {
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
+
+func (p Patient) GetID() string {
+	return p.ID
+}
+
+func (p *Patient) SetID(id string) {
+	p.ID = id
+}
+
+func (p *Patient) SetCreatedAt(t time.Time) {
+	p.CreatedAt = t
+}
+
+func (p *Patient) SetUpdatedAt(t time.Time) {
+	p.UpdatedAt = t
+}
