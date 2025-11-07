6de7397f883f19d308879bfe153f0b2558a29b96
yasintuncer <yasintuncerr@outlook.com>
2025-11-08 01:32:24 +0300

    Add mock generation for service interfaces in generate.go

--- internal/mocks/generate.go
+++ internal/mocks/generate.go
@@ -6,3 +6,4 @@ package mocks
 //go:generate mockgen -destination=repository.go -package=mocks -source=../domain/repository/interface.go
 //go:generate mockgen -destination=storage.go -package=mocks -source=../domain/storage/interface.go
 //go:generate mockgen -destination=publisher.go -package=mocks -source=../domain/events/interface.go Publisher,Subscriber,ImageEventPublisher
+//go:generate mockgen -destination=service.go -package=mocks github.com/histopathai/main-service/internal/service IWorkspaceService,IPatientService,IImageService,IAnnotationService,IAnnotationTypeService
