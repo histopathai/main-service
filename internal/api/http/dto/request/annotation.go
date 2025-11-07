263dccffd2321be8556bdb2148772b11b58001d4
yasintuncer <yasintuncerr@outlook.com>
2025-11-08 02:10:13 +0300

    Update AnnotatorID field to be optional in CreateAnnotationRequest

--- internal/api/http/dto/request/annotation.go
+++ internal/api/http/dto/request/annotation.go
@@ -5,7 +5,7 @@ import "github.com/histopathai/main-service/internal/domain/model"
 // Annotation DTOs
 type CreateAnnotationRequest struct {
 	ImageID     string        `json:"image_id" binding:"required"`
-	AnnotatorID string        `json:"annotator_id" binding:"required"`
+	AnnotatorID string        `json:"annotator_id" binding:"omitempty"`
 	Polygon     []model.Point `json:"polygon" binding:"required,dive"`
 	Score       *float64      `json:"score,omitempty"`
 	Class       *string       `json:"class,omitempty"`
