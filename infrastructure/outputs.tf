a515a0c82671f07a6e2c28e75949e3686d94b2cb
yasintuncer <yasintuncerr@outlook.com>
2025-11-30 18:47:38 +0300

    Add output for processing completed topic in Terraform configuration

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,3 +2,7 @@ output "service_url" {
   description = "The URL of the deployed main service"
   value       = google_cloud_run_v2_service.main_service.uri
 }
+output "processing_completed_topic" {
+  description = "The Pub/Sub topic for image processing results"
+  value       = google_pubsub_topic.image_processing_result.name
+}
