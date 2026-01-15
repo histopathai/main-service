a31ef470e224b22d083c561f549f36b1fb592f9d
yasintuncer <yasintuncerr@outlook.com>
2026-01-16 00:16:12 +0300

    Refactor AnnotationType struct to rename fields for clarity

--- internal/domain/model/annotation_type.go
+++ internal/domain/model/annotation_type.go
@@ -6,11 +6,11 @@ import (
 
 type AnnotationType struct {
 	vobj.Entity
-	Type     vobj.TagType
-	Global   bool
-	Required bool
-	Options  []string
-	Min      *float64
-	Max      *float64
-	Color    *string
+	TagType    vobj.TagType
+	IsGlobal   bool
+	IsRequired bool
+	Options    []string
+	Min        *float64
+	Max        *float64
+	Color      *string
 }
