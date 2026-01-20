72652e00abb5ed50f079e20803a760c31d3dbb3b
yasintuncer <yasintuncerr@outlook.com>
2026-01-20 17:13:18 +0300

    Implement WorkspaceService with Create and Update methods; add type assertions and error handling for command types

--- internal/application/service/workspace.go
+++ internal/application/service/workspace.go
@@ -1,12 +1,18 @@
 package service
 
 import (
+	"context"
+
+	"github.com/histopathai/main-service/internal/application/command"
+	"github.com/histopathai/main-service/internal/application/usecase"
 	"github.com/histopathai/main-service/internal/domain/model"
 	"github.com/histopathai/main-service/internal/port"
+	"github.com/histopathai/main-service/internal/shared/errors"
 )
 
 type WorkspaceService struct {
 	*Service[*model.Workspace]
+	usecase *usecase.WorkspaceUseCase
 }
 
 func NewWorkspaceService(repo port.Repository[*model.Workspace], uowFactory port.UnitOfWorkFactory) *WorkspaceService {
@@ -15,5 +21,42 @@ func NewWorkspaceService(repo port.Repository[*model.Workspace], uowFactory port
 			repo:       repo,
 			uowFactory: uowFactory,
 		},
+
+		usecase: usecase.NewWorkspaceUseCase(repo, uowFactory),
 	}
+}
+
+func (s *WorkspaceService) Create(ctx context.Context, cmd any) (*model.Workspace, error) {
+
+	// Type assertion
+	createCmd, ok := cmd.(command.CreateWorkspaceCommand)
+	if !ok {
+		return nil, errors.NewInternalError("invalid command type for creating workspace", nil)
+	}
+
+	entity, err := createCmd.ToEntity()
+	if err != nil {
+		return nil, err
+	}
+
+	return s.usecase.Create(ctx, entity)
+}
+
+func (s *WorkspaceService) Update(ctx context.Context, cmd any) error {
+
+	// Type assertion
+	updateCmd, ok := cmd.(command.UpdateWorkspaceCommand)
+	if !ok {
+		return errors.NewInternalError("invalid command type for updating workspace", nil)
+	}
+
+	id := updateCmd.GetID()
+	updates := updateCmd.GetUpdates()
+	if updates == nil {
+		return errors.NewValidationError("no updates provided for workspace", map[string]interface{}{
+			"id": id,
+		})
+	}
+
+	return s.usecase.Update(ctx, id, updates)
 }
