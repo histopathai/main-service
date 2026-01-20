372de7d4c4890644fe0b0d18c3478c64d9f41d6e
yasintuncer <yasintuncerr@outlook.com>
2026-01-20 17:13:44 +0300

    Rename Read method to Get in Service interface; update List and ReadByParentID method signatures for consistency

--- internal/port/service.go
+++ internal/port/service.go
@@ -10,13 +10,13 @@ import (
 
 type Service[T Entity] interface {
 	Create(ctx context.Context, cmd command.CreateCommand) (T, error)
-	Read(ctx context.Context, cmd command.ReadCommand) (T, error)
+	Get(ctx context.Context, cmd command.ReadCommand) (T, error)
 	Update(ctx context.Context, cmd command.UpdateCommand) error
 	Delete(ctx context.Context, cmd command.DeleteCommand) error
 	DeleteMany(ctx context.Context, cmd command.DeleteCommands) error
-	List(ctx context.Context, cmd command.ListCommand) (*query.Result[T], error)
+	List(ctx context.Context, cmd command.ListCommand) (*[]query.Result[T], error)
 	Count(ctx context.Context, cmd command.CountCommand) (int64, error)
-	ReadByParentID(ctx context.Context, cmd command.ListCommand) (*query.Result[T], error)
+	GetByParentID(ctx context.Context, cmd command.ReadByParentIDCommand) (*[]query.Result[T], error)
 }
 
 type TelemetryStats struct {
