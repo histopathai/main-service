2fc9a6b0c0d219f0b9cf8df0ad75d90231693344
yasintuncer <yasintuncerr@outlook.com>
2026-01-22 02:31:39 +0300

    Refactor event package: consolidate Serializer interface and remove unused event publisher interfaces

--- internal/port/event.go
+++ internal/port/event.go
@@ -2,9 +2,12 @@ package port
 
 import (
 	"context"
-
-	"github.com/histopathai/main-service/internal/domain/events"
 )
+
+type Serializer interface {
+	Serialize(event interface{}) ([]byte, error)
+	Deserialize(data []byte, v interface{}) error
+}
 
 type Publisher interface {
 	Publish(ctx context.Context, topic string, data []byte, attributes map[string]string) error
@@ -15,16 +18,4 @@ type EventHandler func(ctx context.Context, data []byte, attributes map[string]s
 type Subscriber interface {
 	Subscribe(ctx context.Context, subscription string, handler EventHandler) error
 	Stop() error
-}
-
-// ImageEventPublisher handles image-related event publishing
-type ImageEventPublisher interface {
-	PublishImageProcessingRequested(ctx context.Context, event *events.ImageProcessingRequestedEvent) error
-	PublishImageDeletionRequested(ctx context.Context, event *events.ImageDeletionRequestedEvent) error
-}
-
-// TelemetryEventPublisher handles telemetry event publishing
-type TelemetryEventPublisher interface {
-	PublishTelemetryDLQMessage(ctx context.Context, event *events.DLQMessageEvent) error
-	PublishTelemetryError(ctx context.Context, event *events.TelemetryErrorEvent) error
 }
