edb289e0b788cd0431e14616f97b773637a98854
yasintuncer <yasintuncerr@outlook.com>
2026-01-24 17:53:27 +0300

    feat: Introduce content model and associated services, commands, and value objects, while refactoring entity creation and validation.

---
+++ internal/domain/model/content.go
@@ -0,0 +1,11 @@
+package model
+
+import "github.com/histopathai/main-service/internal/domain/vobj"
+
+type Content struct {
+	vobj.Entity
+	Provider    vobj.ContentProvider
+	Path        string
+	ContentType vobj.ContentType
+	Size        int64
+}
