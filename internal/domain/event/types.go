1f701524d9405bdb8a204baf376da2268185e435
yasintuncer <yasintuncerr@outlook.com>
2026-01-24 17:53:14 +0300

    refactor: simplify `UpdateImageCommand` by extracting content and processing details into a new `Content` entity and associated use cases.

--- internal/domain/event/types.go
+++ internal/domain/event/types.go
@@ -3,8 +3,10 @@ package event
 type EventType string
 
 const (
-	UploadedEventType                 EventType = "uploaded.v1"
-	DeleteEventType                   EventType = "delete.v1"
-	ImageProcessingRequestedEventType EventType = "image.processing.requested.v1"
-	ImageProcessingCompletedEventType EventType = "image.processing.completed.v1"
+	UploadEventType               EventType = "upload.v1"
+	DeleteEventType               EventType = "delete.v1"
+	ImageProcessEventType         EventType = "image.process.request.v1"
+	ImageProcessCompleteEventType EventType = "image.process.complete.v1"
+	ImageProcessDlqEventType      EventType = "image.process.dlq.v1"
+	ImageUploadDlqEventType       EventType = "image.upload.dlq.v1"
 )
