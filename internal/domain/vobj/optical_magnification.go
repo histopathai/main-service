ecd2599b9628a0ef7a5cec87e1fc6a6c23780c7c
yasintuncer <yasintuncerr@outlook.com>
2026-01-24 17:56:05 +0300

    feat: Get Map Method added

--- internal/domain/vobj/optical_magnification.go
+++ internal/domain/vobj/optical_magnification.go
@@ -5,3 +5,17 @@ type OpticalMagnification struct {
 	NativeLevel       *int
 	ScanMagnification *float64
 }
+
+func (om *OpticalMagnification) GetMap() map[string]interface{} {
+	result := make(map[string]interface{})
+	if om.Objective != nil {
+		result["Objective"] = *om.Objective
+	}
+	if om.NativeLevel != nil {
+		result["NativeLevel"] = *om.NativeLevel
+	}
+	if om.ScanMagnification != nil {
+		result["ScanMagnification"] = *om.ScanMagnification
+	}
+	return result
+}
