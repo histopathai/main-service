db2af4c8e74eac1ab057114fbfb0fe408ee6e40a
yasintuncer <yasintuncerr@outlook.com>
2026-04-14 21:29:53 +0300

    feat: add annotation review domain fields, repository, and usecase, and normalize field naming conventions

--- internal/domain/fields/annotation.go
+++ internal/domain/fields/annotation.go
@@ -10,6 +10,8 @@ const (
 	AnnotationPolygon  AnnotationField = "polygon"
 	AnnotationIsGlobal AnnotationField = "is_global"
 	AnnotationColor    AnnotationField = "color"
+	AnnotationResource AnnotationField = "resource"
+	AnnotationReviewID AnnotationField = "review_id"
 )
 
 func (f AnnotationField) APIName() string {
@@ -29,13 +31,17 @@ func (f AnnotationField) DomainName() string {
 	case AnnotationTagType:
 		return "TagType"
 	case AnnotationTagValue:
-		return "TagValue"
+		return "Value"
 	case AnnotationPolygon:
 		return "Polygon"
 	case AnnotationIsGlobal:
-		return "TagGlobal"
+		return "IsGlobal"
 	case AnnotationColor:
-		return "TagColor"
+		return "Color"
+	case AnnotationResource:
+		return "Resource"
+	case AnnotationReviewID:
+		return "ReviewID"
 	default:
 		return ""
 	}
@@ -43,7 +49,7 @@ func (f AnnotationField) DomainName() string {
 
 func (f AnnotationField) IsValid() bool {
 	switch f {
-	case AnnotationTagType, AnnotationTagValue, AnnotationPolygon, AnnotationIsGlobal, AnnotationColor, AnnotationTypeID:
+	case AnnotationTagType, AnnotationTagValue, AnnotationPolygon, AnnotationIsGlobal, AnnotationColor, AnnotationTypeID, AnnotationWsID, AnnotationResource, AnnotationReviewID:
 		return true
 	default:
 		return false
@@ -51,5 +57,5 @@ func (f AnnotationField) IsValid() bool {
 }
 
 var AnnotationFields = []AnnotationField{
-	AnnotationTagType, AnnotationTagValue, AnnotationPolygon, AnnotationIsGlobal, AnnotationColor, AnnotationTypeID,
+	AnnotationTagType, AnnotationTagValue, AnnotationPolygon, AnnotationIsGlobal, AnnotationColor, AnnotationTypeID, AnnotationWsID, AnnotationResource, AnnotationReviewID,
 }
